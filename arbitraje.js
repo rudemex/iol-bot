2b45e60e48c15cd3787bc153b192242e2ca210af
rudemex <mex.skarlatina@gmail.com>
2020-01-08 11:43:04 -0300

    chage ts to ao20

--- arbitraje.js
+++ arbitraje.js
@@ -2,35 +2,35 @@ const iol = require('./node-iol');
 const signale  =  require('signale');
 const utils = require('./utils.js');
 
-//2020-01-08T02:59:59.999Z
-console.log( utils.currentDay() );
+//console.log( utils.currentDay() );
 
-/*iol.auth().then(token => {
+iol.auth().then(token => {
     return Promise.all([
     iol.getTickerValue(token, 'bcba', 'ay24'),
     iol.getTickerValue(token, 'bcba', 'ay24d'),
-    iol.getTickerValue(token, 'bcba', 'TS'),
-    iol.getTickerValue(token, 'nyse', 'TS')]).then(values => {
+    iol.getTickerValue(token, 'bcba', 'ao20'),
+    iol.getTickerValue(token, 'bcba', 'ao20d')]).then(values => {
         let ay24 = values[0];
         let ay24d = values[1];
-
+        let ao20 = values[2];
+        let ao20d = values[3];
 
         let ccl = ay24.ultimoPrecio/ay24d.ultimoPrecio;
-        let tsPriceInArg = values[2].ultimoPrecio;
-        let tsPriceInUsa = values[3].ultimoPrecio;
+        let ao20Price = ao20.ultimoPrecio;
+        let ao20DPric = ao20d.ultimoPrecio;
 
         signale.info('AY24:', ay24.ultimoPrecio);
         signale.info('AY24D', ay24d.ultimoPrecio);
-        signale.info('TS price in ARG', tsPriceInArg);
-        signale.info('TS price in USA', tsPriceInUsa);
+        signale.info('AO20', ao20Price);
+        signale.info('AO20D', ao20DPric);
         signale.info('La compra de dólar bolsa cuesta:', ccl);
 
-        if ( ((((tsPriceInUsa * ccl) / tsPriceInArg) / 2) - 1) > 0.01 ) { //ARBITRAGE!!!
-            let endOfToday = new Date();
-            endOfToday.setHours(23,59,59,999);
-            signale.info('tsPriceInArg: ',tsPriceInArg);
+
+        if ( ((((ao20DPric * ccl) / ao20Price) / 2) - 1) > 0.01 ) { //ARBITRAGE!!!
+            let endOfToday = utils.currentDay().iso.end;
+            signale.info('ao20Price: ',ao20Price);
             signale.info('endOfToday: ',endOfToday);
-            //return iol.buy(token, 'bcba', 'TS', 1, tsPriceInArg, 't0', endOfToday)
+            //return iol.buy(token, 'bcba', 'TS', 1, ao20Price, 't0', endOfToday)
         }
     })
-}).catch(signale.error)*/
+}).catch(signale.error)
